bdf3ecf6635a48294a3ddf482f497938db5e0d84
vallerypulungan <vallerypulungan@student.ub.ac.id>
2025-05-02 17:34:32 +0700

    Implement DocumentationController: add store method for saving documentation with validation and response handling

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Documentation\DocumentationController;
 
-
+Route::post('/documentations', [DocumentationController::class, 'store']);
 Route::post('/login', [LoginController::class, 'loginApi'])->middleware('api');
 Route::post('/logout', [LoginController::class, 'logoutApi']); 
